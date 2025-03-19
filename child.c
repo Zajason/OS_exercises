cc5d1dd7026edbd7bbdd8ef578484e7985c4ec5b
Zajason <zakynthinosiason@gmail.com>
2025-03-19 11:18:52 +0200

    skase

--- child.c
+++ child.c
@@ -39,19 +39,32 @@ void report_status(int sig)
 {
     time_t now = time(NULL);
     char buf[128];
-    int len = snprintf(buf, sizeof(buf), "Child PID: %d, var: %d, Running Time: %ld sec\n", getpid(), var, now - start_time);
+    int len = snprintf(buf, sizeof(buf), "Parent PID: %d Child PID: %d, var: %d, Running Time: %ld sec\n", getppid(), getpid(), var, now - start_time);
     write(STDOUT_FILENO, buf, len);
     alarm(10);
+}
+
+void setup_signal_handler(int sig, void (*handler)(int))
+{
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(sig, &sa, NULL) == -1)
+    {
+        perror("sigaction failed");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main()
 {
     start_time = time(NULL);
 
-    signal(SIGUSR1, handle_signal);
-    signal(SIGUSR2, handle_signal);
-    signal(SIGTERM, handle_signal);
-    signal(SIGALRM, report_status);
+    setup_signal_handler(SIGUSR1, handle_signal);
+    setup_signal_handler(SIGUSR2, handle_signal);
+    setup_signal_handler(SIGTERM, handle_signal);
+    setup_signal_handler(SIGALRM, report_status);
 
     alarm(10);
 
--- os_second.c
+++ os_second.c
@@ -1,34 +1,11 @@
-/*2 vdomades
-argc = 2
-N = atoi(argv[1])
-2 terminal
-pinaka apo paidia me malloc
-for loop me fork
-perimeno 10 sec me signals
-sigalarm
-xrhsimopoiw global variable
-kathe paidi lambanei 4 signal
-waitpid anti gia wait gia na katalavaineis to status
-waitpid(pid, &status, 0)
-prosoxi gia loop oti an termatisiei apo ton patera den to xanadimiourgo mono an to termatisi kapios apo xristis
-kill(pid, SIGNAL)
-while(true) sta paidia
-an termatiso en paidi kai to xanaftiaxei o pateras tha prepi na ektiposo to neo pid
-to neo paidi den tha simpipti sto countdown me ta palia
-stin child.c tha valome ton kodika toy pediou tha to kanoume compile kai me tin execv trexoyme to ektelesimo mesa ston kodika
-giata 10 sec tha valoyme allo ena alarm ston handler toy sigalarm kai ena sto idio to paidi
-kai etsi ginetai mia loupa
-*/
-
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <ctype.h>
-#include <fcntl.h>
 #include <string.h>
+#include <errno.h>
 
 #define EXIT_INVALID_ARGS 1
 #define EXIT_MALLOC_FAILED 2
@@ -41,7 +18,12 @@ pid_t *children;
 
 void write_message(const char *msg)
 {
-    write(STDOUT_FILENO, msg, strlen(msg));
+    ssize_t len = strlen(msg);
+    if (write(STDOUT_FILENO, msg, len) != len)
+    {
+        perror("write failed");
+        exit(EXIT_WRITE_FAILED);
+    }
 }
 
 void handle_child_exit(int sig)
@@ -52,8 +34,15 @@ void handle_child_exit(int sig)
     while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
     {
         char buf[100];
-        int len = snprintf(buf, 100, "Child %d terminated. Restarting...\n", pid);
-        write(STDOUT_FILENO, buf, len);
+        int len = snprintf(buf, sizeof(buf), "Child %d terminated. Restarting...\n", pid);
+        if (len < 0 || len >= sizeof(buf))
+        {
+            write_message("Error formatting message.\n");
+        }
+        else
+        {
+            write_message(buf);
+        }
 
         for (int i = 0; i < n; i++)
         {
@@ -64,14 +53,24 @@ void handle_child_exit(int sig)
                 {
                     char *args[] = {"./child", NULL};
                     execv(args[0], args);
+                    perror("execv failed");
                     exit(EXIT_EXECV_FAILED);
                 }
-                else
+                else if (new_pid > 0)
                 {
                     children[i] = new_pid;
                 }
+                else
+                {
+                    perror("fork failed");
+                }
             }
         }
+    }
+
+    if (pid == -1 && errno != ECHILD)
+    {
+        perror("waitpid failed");
     }
 }
 
@@ -79,14 +78,20 @@ void handle_sigterm(int sig)
 {
     write_message("Received SIGTERM. Terminating...\n");
 
-    // Ignore SIGCHLD temporarily to prevent respawning children
-    signal(SIGCHLD, SIG_IGN);
+    struct sigaction sa_ignore;
+    sa_ignore.sa_handler = SIG_IGN;
+    sigemptyset(&sa_ignore.sa_mask);
+    sa_ignore.sa_flags = 0;
+    sigaction(SIGCHLD, &sa_ignore, NULL);
 
     for (int i = 0; i < n; i++)
     {
         if (children[i] > 0)
         {
-            kill(children[i], SIGTERM);
+            if (kill(children[i], SIGTERM) == -1)
+            {
+                perror("kill failed");
+            }
         }
     }
 
@@ -105,8 +110,24 @@ void forward_signal(int sig)
     {
         if (children[i] > 0)
         {
-            kill(children[i], sig);
+            if (kill(children[i], sig) == -1)
+            {
+                perror("kill failed");
+            }
         }
+    }
+}
+
+void setup_signal_handler(int sig, void (*handler)(int))
+{
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(sig, &sa, NULL) == -1)
+    {
+        perror("sigaction failed");
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -129,14 +150,14 @@ int main(int argc, char *argv[])
     children = malloc(n * sizeof(pid_t));
     if (children == NULL)
     {
-        perror("malloc");
+        perror("malloc failed");
         exit(EXIT_MALLOC_FAILED);
     }
 
-    signal(SIGCHLD, handle_child_exit);
-    signal(SIGTERM, handle_sigterm);
-    signal(SIGUSR1, forward_signal);
-    signal(SIGUSR2, forward_signal);
+    setup_signal_handler(SIGCHLD, handle_child_exit);
+    setup_signal_handler(SIGTERM, handle_sigterm);
+    setup_signal_handler(SIGUSR1, forward_signal);
+    setup_signal_handler(SIGUSR2, forward_signal);
 
     for (int i = 0; i < n; i++)
     {
@@ -145,6 +166,7 @@ int main(int argc, char *argv[])
         {
             char *args[] = {"./child", NULL};
             execv(args[0], args);
+            perror("execv failed");
             exit(EXIT_EXECV_FAILED);
         }
         else if (pid > 0)
@@ -153,7 +175,7 @@ int main(int argc, char *argv[])
         }
         else
         {
-            perror("fork");
+            perror("fork failed");
             free(children);
             exit(EXIT_FORK_FAILED);
         }
@@ -161,7 +183,14 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-        pause();
+        int ret = pause();
+        if (ret == -1 && errno != EINTR)
+        {
+            perror("pause failed");
+            break;
+        }
     }
+
+    free(children);
     return 0;
 }
